e4072a505b61e289d0f013cd3afeecbf6b573b9a
sizer <shahin.reza@brainstation-23.com>
2024-09-18 12:13:47 +0600

    refactor:remove unnecessary file and change variable name

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() {
-  runApp(SourceApp());
+  runApp(const SourceApp());
 }
 
 class SourceApp extends StatelessWidget {
+  const SourceApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Source App',
       home: SourceHomePage(),
     );
@@ -16,17 +18,19 @@ class SourceApp extends StatelessWidget {
 }
 
 class SourceHomePage extends StatefulWidget {
+  const SourceHomePage({super.key});
+
   @override
   _SourceHomePageState createState() => _SourceHomePageState();
 }
 
 class _SourceHomePageState extends State<SourceHomePage> {
   static const platform = MethodChannel('com.example.source_app/deep_link');
-  final String message = 'https://www.gstatic.com/webp/gallery3/1.sm.png';
+  final String imageUrl = 'https://www.gstatic.com/webp/gallery3/1.sm.png';
 
   Future<void> _openDestinationApp() async {
     try {
-      await platform.invokeMethod('openDestinationApp', {'message': message});
+      await platform.invokeMethod('openDestinationApp', {'message': imageUrl});
     } on PlatformException catch (e) {
       print("Failed to open destination app: '${e.message}'.");
     }
@@ -36,12 +40,12 @@ class _SourceHomePageState extends State<SourceHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Source App'),
+        title: const Text('Source App'),
       ),
       body: Center(
         child: ElevatedButton(
           onPressed: _openDestinationApp,
-          child: Text('Go to Destination App'),
+          child: const Text('Go to Destination App'),
         ),
       ),
     );
